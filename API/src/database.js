fd8ac484605283bf5d72ed701e8bb5a8d8a5c9c4
Carlos Arredondo <carlos.arredondoc@usm.cl>
2023-05-21 18:54:03 -0400

    añadiendo archivo de configuracion de la base de datos

--- API/src/database.js
+++ API/src/database.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
 
 async function connect_db() {
-process.env.MONGODB_URI
 
     try {
-      const db = await mongoose.connect("mongodb://root:password@localhost:27017/",{dbName: "Ayudantia3"});
+      const db = await mongoose.connect("mongodb://root:password@localhost:27017/",{dbName: "Ayudantia3"}); //Usar dotenv con variables de entorno, por ejemplo -> process.env.MONGODB_URI
       console.log("Conectado a la base de datos", db.connection.name);
     } catch (error) {
       console.error(error.message);
