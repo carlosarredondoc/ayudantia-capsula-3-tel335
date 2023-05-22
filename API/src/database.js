26528aa9aeb3e183591389c3be69e2f5cf76380d
Carlos Arredondo <carlos.arredondoc@usm.cl>
2023-05-22 17:57:19 -0400

    dockerizando api junto a la base de datos

--- API/src/database.js
+++ API/src/database.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 async function connect_db() {
 
+    //Base de datos en docker "mongodb://root:password@localhost:27017/"
+    //Proyecto dockerizado completo "mongodb://root:password@mongodb:27017/"
+
     try {
-      const db = await mongoose.connect("mongodb://root:password@localhost:27017/",{dbName: "Ayudantia3"}); //Usar dotenv con variables de entorno, por ejemplo -> process.env.MONGODB_URI
+      const db = await mongoose.connect("mongodb://root:password@mongodb:27017/",{dbName: "Ayudantia3"}); //Usar dotenv con variables de entorno, por ejemplo -> process.env.MONGODB_URI
       console.log("Conectado a la base de datos", db.connection.name);
     } catch (error) {
       console.error(error.message);
